046a793ee7f6e564f212d20d0f71c5315f774bca
Enzo Cioppettini <tenstrings5050@gmail.com>
2018-02-10 15:59:44 -0300

    Some methods for Scalar... Vec2 sub

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,30 @@ mod tests {
     use super::math::*;
     #[test]
     fn can_add_2d_vectors() {
-        let a = Vec2(1.0, 1.0);
-        let b = Vec2(2.0, 1.0);
-        assert_eq!(a + b, Vec2(3.0,2.0));
+        let a = Vec2(1., 1.);
+        let b = Vec2(2., 1.);
+        assert_eq!(a + b, Vec2(3., 2.));
+    }
+
+    #[test]
+    fn can_substract_2d_vectors() {
+        let a = Vec2(1., 1.);
+        let b = Vec2(1., 2.);
+        assert_eq!(a - b, Vec2(0., -1.));
     }
 
     #[test]
     fn can_multiply_2d_vectors_by_scalar() {
-        assert_eq!(Vec2(1.0, 1.0) * Scalar(-1.0), Vec2(-1.0, -1.0));
+        assert_eq!(Vec2(1., 1.) * Scalar(-1.), Vec2(-1., -1.));
     }
 
     #[test]
     fn scalar_by_vector() {
-        assert_eq!(Vec2(-1.0, -1.0), Scalar(-1.0) * Vec2(1.0, 1.0))
+        assert_eq!(Vec2(-1., -1.), Scalar(-1.) * Vec2(1., 1.))
     }
 
     #[test]
     fn dot_product() {
-        assert_eq!(Vec2(1.0,2.0).dot( Vec2(2.0,3.0) ), 2.0 + 2.0*3.0);
+        assert_eq!(Vec2(1.,2.).dot( Vec2(2.,3.) ), Scalar(2. + 2.*3.));
     }
 }
--- src/math.rs
+++ src/math.rs
@@ -1,20 +1,17 @@
-
-use std::ops::{Add, Mul};
+use std::ops::{Add, Mul, Sub, Div};
 use num::Float;
 
 #[derive(Debug, PartialEq)]
 pub struct Vec2<T: Float>(pub T,pub T);
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Scalar<T: Float>(pub T);
 
-impl<T: Float> Vec2<T>
-    where T: Mul<Output=T> + Add<Output=T>{
+impl<T: Float> Vec2<T>{
 
-    pub fn dot(self, other: Self) -> T {
-        self.0 * other.0 + self.1 * other.1
+    pub fn dot(self, other: Self) -> Scalar<T> {
+        Scalar(self.0 * other.0 + self.1 * other.1)
     }
-
 }
 
 impl<T: Float> Add for Vec2<T> {
@@ -24,6 +21,17 @@ impl<T: Float> Add for Vec2<T> {
         Vec2(
             self.0 + other.0,
             self.1 + other.1,
+        )
+    }
+}
+
+impl<T: Float> Sub for Vec2<T> {
+    type Output = Vec2<T>;
+
+    fn sub(self, other: Self) -> Vec2<T> {
+        Vec2(
+            self.0 - other.0,
+            self.1 - other.1,
         )
     }
 }
@@ -41,12 +49,49 @@ impl<T: Float> Mul<Scalar<T>> for Vec2<T> {
 }
 
 impl<T: Float> Mul<Vec2<T>> for Scalar<T> {
-    type Output= Vec2<T>;
+    type Output = Vec2<T>;
 
     fn mul(self, vec: Vec2<T>) -> Vec2<T> {
         Vec2(
             vec.0 * self.0,
             vec.1 * self.0,
         )
+    }
+}
+
+//
+//Traits for Scalar `f` Scalar
+//TODO: Make this things a macro (and add others...)
+//
+
+impl<T: Float> Add<Scalar<T>> for Scalar<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Scalar(self.0 + rhs.0)
+    }
+}
+
+impl<T: Float> Sub<Scalar<T>> for Scalar<T> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Scalar(self.0 - rhs.0)
+    }
+}
+
+impl<T: Float> Mul<Scalar<T>> for Scalar<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Scalar(self.0 * rhs.0)
+    }
+}
+
+impl<T: Float> Div<Scalar<T>> for Scalar<T> {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self {
+        Scalar(self.0 / rhs.0)
     }
 }
