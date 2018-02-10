c41568de1fc4d82d7eb9185373658e00b9b029e2
Enzo Cioppettini <tenstrings5050@gmail.com>
2018-02-10 15:24:26 -0300

    Añadido Scalar<T> Añadido trait Float

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,28 @@
 pub mod math;
+extern crate num;
 
 #[cfg(test)]
 mod tests {
     use super::math::*;
     #[test]
     fn can_add_2d_vectors() {
-        let a = Vec2(1, 1);
-        let b = Vec2(2, 1);
-        assert_eq!(a + b, Vec2(3,2));
+        let a = Vec2(1.0, 1.0);
+        let b = Vec2(2.0, 1.0);
+        assert_eq!(a + b, Vec2(3.0,2.0));
     }
 
     #[test]
     fn can_multiply_2d_vectors_by_scalar() {
-        assert_eq!(Vec2(1,1) * (-1), Vec2(-1, -1))
+        assert_eq!(Vec2(1.0, 1.0) * Scalar(-1.0), Vec2(-1.0, -1.0));
+    }
+
+    #[test]
+    fn scalar_by_vector() {
+        assert_eq!(Vec2(-1.0, -1.0), Scalar(-1.0) * Vec2(1.0, 1.0))
+    }
+
+    #[test]
+    fn dot_product() {
+        assert_eq!(Vec2(1.0,2.0).dot( Vec2(2.0,3.0) ), 2.0 + 2.0*3.0);
     }
 }
--- src/math.rs
+++ src/math.rs
@@ -1,9 +1,23 @@
+
 use std::ops::{Add, Mul};
+use num::Float;
 
 #[derive(Debug, PartialEq)]
-pub struct Vec2<T>(pub T,pub T);
+pub struct Vec2<T: Float>(pub T,pub T);
 
-impl<T: Add<Output=T>> Add for Vec2<T> {
+#[derive(Debug, Eq, PartialEq)]
+pub struct Scalar<T: Float>(pub T);
+
+impl<T: Float> Vec2<T>
+    where T: Mul<Output=T> + Add<Output=T>{
+
+    pub fn dot(self, other: Self) -> T {
+        self.0 * other.0 + self.1 * other.1
+    }
+
+}
+
+impl<T: Float> Add for Vec2<T> {
     type Output = Vec2<T>;
 
     fn add(self, other: Self) -> Vec2<T> {
@@ -14,15 +28,25 @@ impl<T: Add<Output=T>> Add for Vec2<T> {
     }
 }
 
-impl<T: Mul<Output=T> + Copy> Mul<T> for Vec2<T> {
+impl<T: Float> Mul<Scalar<T>> for Vec2<T> {
     type Output = Self;
 
-    fn mul(self, scalar: T) -> Vec2<T> {
+    fn mul(self, scalar: Scalar<T>) -> Vec2<T> {
+        let Scalar(s) = scalar;
         Vec2(
-            self.0 * scalar,
-            self.1 * scalar,
+            self.0 * s,
+            self.1 * s,
         )
     }
+}
 
+impl<T: Float> Mul<Vec2<T>> for Scalar<T> {
+    type Output= Vec2<T>;
 
+    fn mul(self, vec: Vec2<T>) -> Vec2<T> {
+        Vec2(
+            vec.0 * self.0,
+            vec.1 * self.0,
+        )
+    }
 }
